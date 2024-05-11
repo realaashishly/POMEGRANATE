93ede29012edfc3d0e7315a97a8d1fdcbe874944
Aashish Singh <11aashish.singh@gmail.com>
2024-05-11 13:27:15 +0530

    more functionality added

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger, Draggable)
+
 
 const imageSlider = document.querySelector(".hero__images-wrapper");
+const container = document.querySelector(".hero__work-slider");
 const scrollImage = document.querySelector(".image-fill");
+const wrapProgress = gsap.utils.wrap(0, 1);
 
-const sizeOfImage = scrollImage.clientWidth;
+var boxWidth = scrollImage.clientWidth;
+
+
+
+
+
+
+// const sizeOfImage = scrollImage.clientWidth;
 
 let tl = gsap.timeline();
 
@@ -18,5 +28,45 @@ tl.to(imageSlider, {
       }
 })
 
+
+
+Draggable.create(imageSlider, {
+  type: "x",
+  bounds: container,
+  inertia: true,
+  
+});
+
+
+function updateProgress() {
+  if (timeLine)
+    timeLine.progress(
+      wrapProgress(gsap.getProperty(proxy, "x") / carouselWidth)
+    );
+};
+
+
+
+
+
+const faqRows = document.querySelectorAll('.faq__row');
+
+    faqRows.forEach(row => {
+        const rowAnswer = row.querySelector('.faq__answer');
+        const rowArrow = row.querySelector('.faq__arrow');
+
+        row.addEventListener('click', () => {
+          const isAnswerVisible = rowAnswer.style.display !== 'none';
+
+            // Toggle answer visibility
+            if (isAnswerVisible) {
+                        rowAnswer.style.display = 'none';
+                        rowArrow.style.transform = 'rotate(0deg)';
+                    } else {
+                        rowAnswer.style.display = 'block';
+                        rowArrow.style.transform = 'rotate(180deg)';
+                    }
+        });
+    });
 
 
